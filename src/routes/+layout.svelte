595819e457e5c823e8235084f733df038bbdb59a
Miroslav Nožička <miroslav.nozicka@geekcoders.cz>
2025-02-23 01:19:23 +0100

    move to Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<header>
+	<nav>
+		<a href="#projects">Projekty</a><a href="#experinces">Zkušenosti</a><a href="#contact"
+			>Kontakt</a
+		><a href="#certs">Certifikace</a><a href="#tech">Technologie</a>
+	</nav>
+</header>
+<main>{@render children()}</main>
+<footer>&copy; {new Date().getFullYear()} Miroslav Nožička</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,127 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<article>
+	<section style="display: block;margin: auto; width: 50%;">
+		<h2 id="contact">Kontakt</h2>
+		<ul>
+			<li><b>Miroslav Nožička</b></li>
+			<li>IČO: 14404826, neplátce DPH</li>
+			<li>email: miroslav.nozicka@outlook.com</li>
+			<li>Sídlo: Hornická 35, Staré Město 78832</li>
+			<li>Podnikatel vedený u Živnostenského úřadu v Šumperku</li>
+		</ul>
+		<img width="80%" src="/images/port.jpg" alt="Portrait" />
+	</section>
+	<section>
+		<h2 id="projects">Projekty</h2>
+		<p>Seznam projektů, které jsem vytvořil ve volném čase.</p>
+		<h3><a href="https://eligma.cz/">Eligma</a></h3>
+		<div>
+			<p>Online nástroj pro přátele skauty, který umožňuje snadno šifrovat/kódovat text.</p>
+			<img width="100%" src="/images/eligma.png" alt="web eligma" />
+			<h3><a href="https://stredisko-sneznik.skauting.cz/">Středisko Sněžník</a></h3>
+			<img width="100%" src="/images/sneznik.png" alt="web středisko sněžník" />
+			<p>
+				Na tento projekt jsem z technologické hlediska hrdý. Na první pohled se nejedná o nijak
+				zajímavou webovou stránku, ale na pozadí se dějí zajímavé věci.
+			</p>
+			<p>
+				Stránky jsou statické, ale přesto mají administraci ve které mohou uživatelé upravovat a
+				přidávat nový obsah. Stránky využívají <a href="https://decapcms.org/">Decap CMS</a> s
+				Github Backendem pro editaci stránkek a zachování historie. S trochou PHP na propojení
+				(Github OAUTH). CMS pushne změny na git, Github actions buildnout změny S pomocí generátoru
+				statických stránek <a href="https://gohugo.io/">Hugo</a> a výsledek se přes FTP nahraje na server.
+			</p>
+			<h3><a href="https://quickcards.eu/cs/">Quickcards</a></h3>
+			<p>
+				Jednoduchý nástroj pro tvorbu studijních kartiček. Hlavně pro osbní účely při studiu na
+				certifikáty.
+			</p>
+			<img width="100%" src="/images/quickcards.png" alt="web quickcards" />
+			<h3>2D grafika</h3>
+			<p>
+				Mimo programování se snažím zlepšovat i v designu a grafice. Především kvůli tvorbě her.
+			</p>
+			<h4>Pixel art</h4>
+			<ul class="two-cols">
+				<li>
+					<a href="https://lowich.itch.io/slime-platformer-tileset">Platformer asset pack</a>
+					<iframe
+						title="Platformer"
+						frameborder="0"
+						src="https://itch.io/embed/2007821"
+						width="90%"
+						height="167"
+						><a href="https://lowich.itch.io/slime-platformer-tileset"
+							>Slime platformer tileset by Lowich</a
+						></iframe
+					>
+				</li>
+				<li>
+					<a href="https://lowich.itch.io/free-spaceship-sprites">Shoot'em up asset pack</a>
+					<iframe
+						title="Space ships"
+						frameborder="0"
+						src="https://itch.io/embed/1839661"
+						width="90%"
+						height="167"
+						><a href="https://lowich.itch.io/free-spaceship-sprites">Spaceship sprites by Lowich</a
+						></iframe
+					>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<div class="two-cols">
+		<section>
+			<h2>Zkušenosti</h2>
+			<h3>Golang developer: <a href="https://www.mall.cz/">Mall.cz</a></h3>
+			<ul>
+				<li>Správa a vývoj několika Golang micro service</li>
+				<li>Značné optimalizace a přechod na asynchroní zpracování</li>
+				<li>Monitoring, alerting a stabilizace služeb</li>
+			</ul>
+			<h3>Python developer: <a href="https://fameless.cz/">Fameless.cz</a></h3>
+			<ul>
+				<li>Tvorba komplexního GraphQL API pro komunikaci s Pohodou</li>
+				<li>Implementace Django aplikace s administrací a komunikací přes RabbitMQ</li>
+				<li>Implementace programu pro koncová Windows zařízení se správou Windows služeb</li>
+			</ul>
+			<h3>Backend developer: <a href="https://geekcoders.cz/">Geekcoders.cz</a></h3>
+			<ul>
+				<li>Práce na PHP, Python a Golang projetech, příležitostná Dev/Ops výpomoc</li>
+				<li>Konfigurace a údržba serveru pro interní služby</li>
+			</ul>
+		</section>
+		<div>
+			<section>
+				<h2 id="certs">Certifikace</h2>
+				<ul>
+					<li>
+						<a href="https://www.credly.com/badges/0942dd0a-7e72-4ba3-b45c-6514035034bd/public_url"
+							>Comptia Security+</a
+						>
+					</li>
+					<li>
+						<a href="https://skillshop.exceedlms.com/student/award/wJyxjacTNbVyXGsnxqmQPoXJ"
+							>Fundamentals of Digital Marketing</a
+						>
+					</li>
+					<li>
+						<a href="https://courses.edx.org/certificates/28ffc856d0194eaba4a91b6e0316bc34"
+							>DAT101x: Microsoft Professional Orientation : Data Science</a
+						>
+					</li>
+				</ul>
+			</section>
+			<section>
+				<h2 id="tech">Technologie a nástroje</h2>
+				<ul>
+					<li>Frontend: HTML, CSS, JS, Svelte, Alpine.js</li>
+					<li>Backend: Python, Golang, PHP</li>
+					<li>Databáze: MySQL, MSSQL, MariaDB, MongoDB</li>
+					<li>DevOps: Docker, Github Actions, Gitlab pipelines, Bitbucket pipelines</li>
+					<li>Ostatní: Linux, Git, RabbitMQ, GraphQL</li>
+				</ul>
+			</section>
+		</div>
+	</div>
+</article>
